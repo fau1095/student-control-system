1ec8b4d3b1af58b275bb034d6aeccf446547aaa5
Fabrizio Méndez <fabriziomendezalberti@gmail.com>
2025-05-27 02:25:52 -0600

    Fixes as requested by Jean Carlo

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from menu import show_menu
+from typing import List, Dict, Any
+
+students: List[Dict[str, Any]] = []  # Define students list as list of dicts
 
 if __name__ == "__main__":
-    show_menu()
+    show_menu(students)  # Pass students to show_menu
